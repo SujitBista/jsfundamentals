b3ccfbfed513297976ba14e7518dd1a328888d0f
sujit bista <sujitbistaprogrammer@gmail.com>
2019-08-19 22:05:23 +0545

    let and const es6 feature

---
+++ script.js
@@ -0,0 +1,14 @@
+// Lecture: let and const
+
+// ES5 
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+const name6 = 'Jane Smith';
+let age5 = 23;
+//name6 = 'Jane Miller';
+age5 = 24;
+console.log(age5);
