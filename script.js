8f9db0c0b3d4c9aaf3888e719d8b5379e64ffe7b
sujit bista <sujitbistaprogrammer@gmail.com>
2019-08-20 20:50:27 +0545

    es5 driverLicence function

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ console.log(name5);
 
 // ES6
 const name6 = 'Jane Smith';
-let age5 = 23;
+var age5 = 23;
 //name6 = 'Jane Miller';
 age5 = 24;
 console.log(age5);
+
+//ES 5
+function driverLicence(passedTest){
+    if(passedTest){
+        var firstName = 'Jeena';
+        var yearOfBirth = '1991';
+        document.write(firstName + 
+        ', born in' +
+        yearOfBirth + 
+        ' ,is now officially allowed to drive a car');
+    }
+}
+
+driverLicence(true);
