b3f2141d49e410fc9b8f53ce8d153bd23c8b7b22
sujit bista <sujitbistaprogrammer@gmail.com>
2019-09-08 09:01:33 +0545

    created prototype property from Object.create

--- script.js
+++ script.js
@@ -233,3 +233,36 @@ console.log(john.lastName);
 john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
+
+//Object.create
+var personProto = {
+    calculateAge: function() {
+        console.log(2019 -
+            this.yearOfBirth);
+    }
+}
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = '1991';
+
+
+function getRecipe(){
+    setTimeout(() => {
+        const recipeID = [523,883,432,974];
+        console.log(recipeID);
+        setTimeout(id => {
+            const recipe = {title: 'Fresh Tomato Pasta',
+            publisher: 'Jonas'};
+            console.log(`${id}: ${recipe.title}`);
+            setTimeout(publisher => {
+                const recipe2 = {title: 'Italian Pizza',
+                publisher: `${publisher}`};
+                console.log(recipe2);
+            },1500,recipe.publisher);
+        },1500,recipeID[2]);
+    }, 1500);
+}   
+
+getRecipe();
+
